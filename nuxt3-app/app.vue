e6d7ad816696ab716f04eadd01a8f16015bfc158
diopei <diopei@gangutech.com>
2022-11-14 12:08:08 +0800

    [nuxt]新創頁面及超連結

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
     <NuxtPage></NuxtPage>
+    <NuxtLink to="/">首页</NuxtLink>
+    <NuxtLink to="/admin">admin</NuxtLink>
   </div>
 </template>
---
+++ nuxt3-app/pages/admin.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>我是admin</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "admin"
+}
+</script>
+
+<style scoped>
+
+</style>
