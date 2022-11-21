b998617e70574f73637172a80e753b4b2817c598
diopei <diopei@gangutech.com>
2022-11-21 15:39:41 +0800

    [nuxt]使用樣板，未整理樣式

--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,35 +1,29 @@
 <template>
-<div class="homePage">
-  <h1>我是首頁</h1>
-  <img src="~/assets/images/dd.jpg" />
-  <div id="scroll-zone">
-    <header data-scroll data-scroll-sticky data-scroll-target="#scroll-zone">
-      簡單的滾動效果。
-    </header>
-    <img class="hero-image" src="https://it-monk.com/wp-content/uploads/2021/02/1614099723447.png"/>
-    <div class="title-container" data-scroll-container>
-      <header data-scroll data-scroll-sticky data-scroll-target=".title-container" class="header-titles" data-scroll-offset="-60">
-        然後要有點「 微妙」
-      </header>
-      <h1 data-scroll data-scroll-speed="-2">蝦咪!</h1>
-      <h2 data-scroll data-scroll-speed="1">微妙!</h2>
-      <div data-scroll data-scroll-speed="1.5"  data-scroll-direction="horizontal">
-        <h3 data-scroll data-scroll-speed="2">google 關鍵字:微妙 </h3>
-      </div>
-      <div data-scroll data-scroll-speed="1.5">
-        <h3 data-scroll data-scroll-speed="-1" data-scroll-direction="horizontal">《老子．第一五章》</h3>
-        <p>古之善為士者，<strong>微妙</strong>元通，深不可識。</p>
-      </div>
-      <h1 data-scroll data-scroll-speed="-1"  data-scroll-direction="horizontal">哩西勒</h1>
-      <h1 data-scroll data-scroll-speed="1" data-scroll-direction="horizontal">公三小</h1>
-      <img width=200 src="https://images.unsplash.com/photo-1635231701198-214ad1b687c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3087&q=80"
-           data-scroll data-scroll-speed="3"/>
+  <div class="homePage">
+    <div class="container">
+      <section data-bgcolor="#bcb8ad" data-textcolor="#032f35">
+        <div>
+          <h1 data-scroll data-scroll-speed="1"><span>Horizontal</span> <span>scroll</span> <span>section</span></h1>
+          <p data-scroll data-scroll-speed="2" data-scroll-delay="0.2">with GSAP ScrollTrigger & Locomotive Scroll</p>
+        </div>
+
+      </section>
+
+      <section id="sectionPin">
+        <div class="pin-wrap">
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+          <img src="https://images.pexels.com/photos/5207262/pexels-photo-5207262.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
+          <img src="https://images.pexels.com/photos/3371358/pexels-photo-3371358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
+          <img src="https://images.pexels.com/photos/3618545/pexels-photo-3618545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
+
+        </div>
+      </section>
+      <section data-bgcolor="#e3857a" data-textcolor="#f1dba7"><img src="https://images.pexels.com/photos/4791474/pexels-photo-4791474.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+        <h2 data-scroll data-scroll-speed="1" class="credit"><a href="https://thisisadvantage.com" target="_blank">Made by Advantage</a></h2>
+      </section>
     </div>
 
-    <img class="hero-image" src="https://it-monk.com/wp-content/uploads/2021/02/1614015294135.png"/>
   </div>
-
-</div>
 </template>
 
 <script>
--- nuxt3-app/plugins/locomotive-scroll.client.js
+++ nuxt3-app/plugins/locomotive-scroll.client.js
@@ -2,20 +2,70 @@
 import { defineNuxtPlugin } from "#app";
 import LocomotiveScroll from 'locomotive-scroll';
 import '~/assets/css/locomotive.css'
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector("#scroll-zone"),
-    smooth: true,
-    lerp: 0.2,
-    repeat: true,
-    tablet:{
-        smooth:true,
-        breakpoint:750
-    },
+gsap.registerPlugin(ScrollTrigger);
+
+const pageContainer = document.querySelector(".container");
+
+/* SMOOTH SCROLL */
+const scroller = new LocomotiveScroll({
+    el: pageContainer,
+    smooth: true
 });
+
+scroller.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(pageContainer, {
+    scrollTop(value) {
+        return arguments.length
+            ? scroller.scrollTo(value, 0, 0)
+            : scroller.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+        return {
+            left: 0,
+            top: 0,
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    },
+    pinType: pageContainer.style.transform ? "transform" : "fixed"
+});
+
+////////////////////////////////////
+////////////////////////////////////
+window.addEventListener("load", function () {
+    let pinBoxes = document.querySelectorAll(".pin-wrap > *");
+    let pinWrap = document.querySelector(".pin-wrap");
+    let pinWrapWidth = pinWrap.offsetWidth;
+    let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+
+    // Pinning and horizontal scrolling
+
+    gsap.to(".pin-wrap", {
+        scrollTrigger: {
+            scroller: pageContainer, //locomotive-scroll
+            scrub: true,
+            trigger: "#sectionPin",
+            pin: true,
+            // anticipatePin: 1,
+            start: "top top",
+            end: pinWrapWidth
+        },
+        x: -horizontalScrollLength,
+        ease: "none"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+    ScrollTrigger.refresh();
+});
+
 
 // 文档
 // https://github.surmon.me/vue-awesome-swiper
 export default defineNuxtPlugin((nuxtApp) => {
-    return nuxtApp.vueApp.use(scroll);
+    return nuxtApp.vueApp.use(scroller,pageContainer);
 });
