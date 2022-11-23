dfd20ed90049dca083b122e3f1b2e4fc1e8deacd
diopei <diopei@gangutech.com>
2022-11-23 15:14:04 +0800

    [nuxt]整理首頁css樣式

--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="homePage">
     <div class="container">
-      <section data-bgcolor="#bcb8ad" data-textcolor="#032f35">
+      <section class="wellcome" data-bgcolor="#bcb8ad" data-textcolor="#032f35">
         <LineBackground />
-        <div>
-          <h1 data-scroll data-scroll-speed="1"><span>Horizontal</span> <span>scroll</span> <span>section</span></h1>
-          <p data-scroll data-scroll-speed="2" data-scroll-delay="0.2">with GSAP ScrollTrigger & Locomotive Scroll</p>
-        </div>
-
+        <h1 data-scroll data-scroll-speed="1"><span>Horizontal</span> <span>scroll</span> <span>section</span></h1>
+        <p data-scroll data-scroll-speed="2" data-scroll-delay="0.2">with GSAP ScrollTrigger & Locomotive Scroll</p>
       </section>
 
       <section id="sectionPin">
@@ -19,7 +16,7 @@
 
         </div>
       </section>
-      <section data-bgcolor="#e3857a" data-textcolor="#f1dba7"><img src="https://images.pexels.com/photos/4791474/pexels-photo-4791474.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+      <section class="section-three" data-bgcolor="#e3857a" data-textcolor="#f1dba7"><img src="https://images.pexels.com/photos/4791474/pexels-photo-4791474.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
         <h2 data-scroll data-scroll-speed="1" class="credit"><a href="#" target="_blank">Made by Diopei</a></h2>
       </section>
     </div>
