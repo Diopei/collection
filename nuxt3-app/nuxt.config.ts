f082e4211bf9c88a90a45692c03bcfa590b79367
diopei <diopei@gangutech.com>
2022-12-14 15:01:36 +0800

    [nuxt]修改head的配置到config裏面

--- nuxt3-app/nuxt.config.ts
+++ nuxt3-app/nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        // 配置网页头部 以下是默认值
+        head: {
+            "charset": "utf-8",
+            "viewport": "width=device-width, initial-scale=1",
+            "meta": [
+                { name: 'description', content: 'My amazing site.' },
+            ],
+            "link": [],
+            "style": [],
+            "script": [],
+            bodyAttrs: {
+                class: 'test123444'
+            },
+        },
+    },
     css: [
         // SCSS file in the project
         '~/assets/css/main.scss',
