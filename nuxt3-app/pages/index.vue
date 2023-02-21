3b3f2d5d92704d817625603cff0056ba47bf2e79
diopei <diopei@gangutech.com>
2023-02-21 14:05:18 +0800

    [nuxt]安裝swiper 調整作品集呈現方式

--- nuxt3-app/pages/index.vue
+++ nuxt3-app/pages/index.vue
@@ -6,15 +6,11 @@
         <h1 data-scroll data-scroll-speed="1"><span>Welcome</span> <span>to my</span> <span>Website</span></h1>
         <p data-scroll data-scroll-speed="2" data-scroll-delay="0.2">using Nuxt3 {{store.msg}}</p>
       </section>
-
-      <section id="sectionPin">
-        <div class="pin-wrap">
+      <section>
           <h3>Like most front-end engineers, I have experienced generations without RWD, starting from traditional development, experiencing the separation of front and back ends, and still learning new technologies</h3>
           <img src="https://images.pexels.com/photos/5207262/pexels-photo-5207262.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
           <img src="https://images.pexels.com/photos/3371358/pexels-photo-3371358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
           <img src="https://images.pexels.com/photos/3618545/pexels-photo-3618545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
-
-        </div>
       </section>
       <section>
         <GlitchArtBackground />
