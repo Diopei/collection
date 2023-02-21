37efd4665be0ad03d05230e4a8573c343a3602d8
diopei <diopei@gangutech.com>
2023-02-21 17:57:26 +0800

    [nuxt]作品集位置內容調整完畢

--- nuxt3-app/components/Portfolio.vue
+++ nuxt3-app/components/Portfolio.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="portfolio">
-    <swiper class="swiper">
-      <swiper-slide class="slide">Slide 1</swiper-slide>
-      <swiper-slide class="slide">Slide 2</swiper-slide>
-      <swiper-slide class="slide">Slide 3</swiper-slide>
-      <swiper-slide class="slide">Slide 4</swiper-slide>
-      <swiper-slide class="slide">Slide 5</swiper-slide>
-      <swiper-slide class="slide">Slide 6</swiper-slide>
-      <swiper-slide class="slide">Slide 7</swiper-slide>
-      <swiper-slide class="slide">Slide 8</swiper-slide>
+    <swiper class="swiper"
+            :style="{
+              '--swiper-navigation-color': '#fff',
+              '--swiper-pagination-color': '#fff'
+            }"
+            :modules="modules"
+            direction="vertical"
+            :speed="600"
+            :parallax="true"
+            :pagination="{ clickable: true }"
+    >
+      <swiper-slide class="slide">
+        <img src="https://images.pexels.com/photos/5207262/pexels-photo-5207262.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
+        <div class="info">簡介1 <NuxtLink class="link" target="_blank" to="https://google.com.tw/">look it!</NuxtLink></div>
+      </swiper-slide>
+      <swiper-slide class="slide">
+        <img src="https://images.pexels.com/photos/3371358/pexels-photo-3371358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
+        <div class="info">簡介2</div>
+      </swiper-slide>
+      <swiper-slide class="slide">
+        <img src="https://images.pexels.com/photos/3618545/pexels-photo-3618545.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=900" alt="">
+        <div class="info">簡介3</div>
+      </swiper-slide>
     </swiper>
   </div>
 </template>
@@ -16,7 +30,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Parallax, Pagination, Navigation } from 'swiper'
 import 'swiper/css'
+import 'swiper/css/pagination'
 
 export default defineComponent({
   name: 'Portfolio',
@@ -25,6 +41,11 @@ export default defineComponent({
   components: {
     Swiper,
     SwiperSlide
+  },
+  setup() {
+    return {
+      modules: [Parallax, Pagination]
+    }
   }
 })
 </script>
